f221d7facf2ea1395d5ecef9e1f9c853c8d07c48
Bastian Deutsche <masmoebel2025@gmail.com>
2026-05-02 18:30:02 +0200

    Add Netlify configuration for forms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import netlify from 'astro-netlify';
 
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()],
     css: {
